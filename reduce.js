7c471503a31ba7acd429ee0a979d1c50e287e872
nedduc <marinacudden@gmail.com>
2021-10-25 16:48:48 +0000

    the reduce() method

--- reduce.js
+++ reduce.js
@@ -6,6 +6,17 @@
 
 
 // Summing an array of numbers:
+const nums = [0, 1, 2, 3, 4];
+let sum = nums.reduce((acc, curr) => {
+console.log(
+  "Accumulator:", acc,
+  "Current Value:", curr,
+  "Total:", acc + curr,
+);
+return acc + curr;
+}, 10);
+console.log(sum);
+
 
 
 const teamMembers = [
